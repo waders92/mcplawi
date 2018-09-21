f8b68367461b3c048bf24b99d53df5c9bdfbf870
waders92 <wade.carlson@pga.com>
2018-09-20 20:38:52 -0500

    added routing for events as well as an event index and new event form. Also added restrictions for admin on creationg events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!, only: %i(new create edit update destroy)
+
+    def index
+        @events = Event.all
+    end
+
+    def new
+        admin_required
+        @event = Event.new
+    end
+
+    private
+
+    def admin_required
+        unless current_user.admin?
+            redirect_to root_path
+            flash[:alert] = 'You are not authorized to add an event!'
+        end  
+    end
 end
