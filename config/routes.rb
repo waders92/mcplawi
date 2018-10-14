b004c8feaddd17f173409900bd11281d1ba50003
waders92 <wade.carlson@pga.com>
2018-10-14 13:32:16 -0500

    added dashboard path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :dashboards, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'mcplinks#index'
   get '/clubs', to: 'mcplinks#clubs'
