4c045b5a77df6664bf5534eaef514b8b9b0ad337
waders92 <wade.carlson@pga.com>
2018-12-30 17:23:24 -0600

    turbolinks fix for jquery datepicker

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,4 +1,4 @@
-$('document').ready(function() {
+document.addEventListener("turbolinks:load", function() {
   $('.datepicker').datepicker({
     todayHighlight: true, 
     orientation: 'auto top', 
