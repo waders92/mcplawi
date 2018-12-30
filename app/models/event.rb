be717d6c22a410e2229810d82b61300f8f5f9b36
waders92 <wade.carlson@pga.com>
2018-12-30 11:30:37 -0600

    fixing heroku error

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,13 @@
 class Event < ApplicationRecord
   belongs_to :user
+  mount_uploader :winners_picture, ImageUploader
   has_many :registrations, dependent: :destroy
+
   validates :event_title, presence: true, length: { minimum: 1 }
   validates :event_date, presence: true
   validates :event_location, presence: true
   validates :event_cost, presence: true
   validates :event_start_time, presence: true
-  validates :blue_golf_link, presence: true
 
   def total_registrations
     registrations.count > 0 ? registrations.count : 0
