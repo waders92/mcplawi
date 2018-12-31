683056d6d2f083bff7d655f7ff14c681c119c2ee
waders92 <wade.carlson@pga.com>
2018-12-31 12:40:18 -0600

    css cleanup with notifications

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,8 +1,13 @@
 class Registration < ApplicationRecord
   belongs_to :event
   belongs_to :user
+  after_create :send_registration_email
 
   def registration_time
     self.created_at.strftime('%b %e, %l:%M %p')
+  end
+
+  def send_registration_email
+    NotificationMailer.registration_added(self).deliver_now
   end
 end
