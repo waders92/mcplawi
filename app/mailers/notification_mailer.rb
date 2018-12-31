d956040b7ce6c0fb67fc1a859cfb7f8a101b4f31
waders92 <wade.carlson@pga.com>
2018-12-31 12:09:58 -0600

    mailers configured

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,12 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@mcplawi.com"
 
-  def registration_added
-    mail(to: "wcarlsondev@gmail.com",
-      subject: "A registration has been added for a MCPLAWI event!")
+  def registration_added(event)
+    @registration = event
+    @registered_event = @registration.event
+    @event_registrant = @registration.user
+
+    mail(to: @event_registrant.email,
+      subject: "A registration has been added for a MCPLAWI event, the #{@registered_event.event_title}")
   end
 end
