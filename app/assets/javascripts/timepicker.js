246b80d2abf548b7cef23baffb9d5795e367b5f5
waders92 <wade.carlson@pga.com>
2019-01-05 21:28:10 -0600

    update jquery timepicker params

--- app/assets/javascripts/timepicker.js
+++ app/assets/javascripts/timepicker.js
@@ -2,10 +2,10 @@ document.addEventListener("turbolinks:load", function() {
   $('.timepicker').timepicker({
     timeFormat: 'h:mm p',
     interval: 30,
-    minTime: '10',
+    minTime: '06',
     maxTime: '6:00pm',
     defaultTime: '8',
-    startTime: '10:00',
+    startTime: '06:00',
     dynamic: false,
     dropdown: true,
     scrollbar: true,
