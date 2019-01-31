b921614c2ce3d3bf04bf0636a26705e560620b75
waders92 <wade.carlson@pga.com>
2019-01-30 21:21:30 -0600

    adding partners to emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,6 +6,7 @@ class NotificationMailer < ApplicationMailer
     @registered_event = @registration.event
     @event_registrant = @registration.user
     @event_fee = @event_registrant.event_cost(@registration.event)
+    @player_partner = @event_registrant.partner.first_name + " " + @event_registrant.partner.last_name
 
     mail(to: @event_registrant.email,
          subject: "A registration has been added for a MCPLAWI event, the #{@registered_event.event_title}")
@@ -17,6 +18,7 @@ class NotificationMailer < ApplicationMailer
     @event_registrant = @registration.user
     @event_fee = @event_registrant.event_cost(@registration.event)
     @users_club = @event_registrant.home_club
+    @player_partner = @event_registrant.partner.first_name + " " + @event_registrant.partner.last_name
     mail(to: User.admin_users,
          subject: "MCPLA ADMIN - A registration has been added for a MCPLAWI event, the #{@registered_event.event_title}")
   end
