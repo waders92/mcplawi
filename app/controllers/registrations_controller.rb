728af10507ea6fbde165efe3401e36ad8be6d28a
waders92 <wade.carlson@pga.com>
2019-02-28 20:04:53 -0600

    stripe validation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,7 @@ class RegistrationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @first_count = Registration.count
     current_user.registrations.create(event: current_event)
 
     @amount = current_user.event_cost(current_event) * 100.to_i
@@ -20,14 +21,28 @@ class RegistrationsController < ApplicationController
 
     flash[:notice] = 'You have registered for the event!'
     redirect_to confirmation_path
-  rescue Stripe::CardError => e
+
+    rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to root_path
+    @second_count = Registration.count
+    check_for_valid_registration(@first_count, @second_count)
+    redirect_to event_path(current_event)
   end
 
   private
 
   def current_event
     @current_event ||= Event.find(params[:event_id])
+  end
+
+  def deny_registration
+    @registration = Registration.last
+    @registration.destroy
+  end
+
+  def check_for_valid_registration(first, second)
+    if second > first
+      deny_registration
+    end
   end
 end
