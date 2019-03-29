689f05905bbe2e54da63834f46eb5a73c1496601
waders92 <wade.carlson@pga.com>
2019-03-29 15:27:20 -0500

    added minute testing

--- spec/controllers/minutes_controller_spec.rb
+++ spec/controllers/minutes_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe MinutesController, type: :controller do
+  describe 'minutes#index action' do
+    it 'should render te index page' do
+    get :index
+    expect(response).to have_http_status(:success)
+    end
+  end
 
+  describe 'minutes#new action' do
+    it 'should redirect non-admin users to the root page' do
+      user = FactoryBot.create(:user)
+      sign_in user
+
+      get :new
+      expect(response).to redirect_to root_path
+    end
+
+    it 'should render the new page template to admin users' do
+      user = User.create(
+        email: 'fakeuser@gmail.com',
+        password: 'secretPassword',
+        password_confirmation: 'secretPassword',
+        admin: 'true'
+      )
+      sign_in user
+
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
