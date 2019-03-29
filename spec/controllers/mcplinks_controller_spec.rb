247372b7a148911377bda054802707d68d722ff7
waders92 <wade.carlson@pga.com>
2019-03-29 14:19:34 -0500

    added additional mcplawi controller tests

--- spec/controllers/mcplinks_controller_spec.rb
+++ spec/controllers/mcplinks_controller_spec.rb
@@ -14,4 +14,55 @@ RSpec.describe McplinksController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'mcplawi#officers action' do
+    it 'should render the officers page' do
+      get :officers
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'mcplawi#contact action' do
+    it 'should render the contact page' do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'mcplawi#confirmation action' do
+    it 'should render the confirmation page' do
+      get :confirmation
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'mcplawi#players action' do
+    it 'should render the players page' do
+      get :players
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'mcplawi#admin action' do
+    it 'should require the user to be an admin' do
+      user = FactoryBot.create(:user)
+      sign_in user
+
+      get :admin
+      expect(response).to redirect_to root_path
+    end
+
+    it 'should render the admin page if the user is an admin' do
+      user = User.create(
+        email: 'fakeuser@gmail.com',
+        password: 'secretPassword',
+        password_confirmation: 'secretPassword',
+        admin: 'true'
+      )
+      sign_in user
+      
+      get :admin
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
--- spec/controllers/partners_controller_spec.rb
+++ spec/controllers/partners_controller_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe PartnersController, type: :controller do
   describe 'partners#new action' do
     it 'should show the new partner form if the user is logged in' do
       user = FactoryBot.create(:user)
+      event = FactoryBot.create(:event, event_title: "Title 1")
       sign_in user
 
-      get :new
+      get :new, params: { event_id: event.id }
       expect(response).to have_http_status(:ok)
     end
   end
