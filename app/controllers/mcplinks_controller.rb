c636ad3c23066fc53ccf618a6a08c3b49cb936ae
waders92 <wade.carlson@pga.com>
2019-05-27 15:25:58 -0500

    added result history section

--- app/controllers/mcplinks_controller.rb
+++ app/controllers/mcplinks_controller.rb
@@ -8,6 +8,11 @@ class McplinksController < ApplicationController
 
   def contact; end
 
+  def result_history
+    @events = Event.all.order('created_at ASC').group_by(&:year)
+    # @events = Event.includes(:photo).find(params[:id])
+  end
+
   def players
     @users = if params[:term]
                User.where('LOWER(last_name) LIKE ?', "%#{params[:term].downcase}%").order('last_name ASC').paginate(page: params[:page], per_page: 10)
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,4 +6,12 @@ class Photo < ApplicationRecord
   validates :flight_winner, presence: true, length: { minimum: 1 }
   validates :winning_score, numericality: { greater_than: 0 }
 
+  def year
+    created_at.strftime('%Y')
+  end
+
+  def date
+    result_date.strftime('%b %d, %Y')
+  end
+
 end
