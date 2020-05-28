4c31458d8bf80e52a90ec7f36bf6e1f72f993e14
waders92 <wade.carlson@pga.com>
2020-05-27 20:54:43 -0500

    updating image uploading and landing page content

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [350, 350], if: :is_image?
+  process resize_to_limit: [500, 500], if: :is_image?
   #
   # def scale(width, height)
   #   # do something
