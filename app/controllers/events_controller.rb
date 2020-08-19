b648f647fb138c8992f9a330e5f4eefd373a2a0f
waders92 <wade.carlson@pga.com>
2020-08-19 13:39:28 -0500

    swap event order

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create show edit update destroy]
 
   def index
-    events = Event.all.order("created_at ASC")
+    events = Event.all.order("created_at DESC")
     yearly_events = get_current_year_events(events)
     @events = yearly_events
   end
