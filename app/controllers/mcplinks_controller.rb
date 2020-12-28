2723df37283a289b943032be5527cf67172a295a
waders92 <wade.carlson@pga.com>
2020-12-28 10:42:41 -0600

    finsh up officers

--- app/controllers/mcplinks_controller.rb
+++ app/controllers/mcplinks_controller.rb
@@ -9,14 +9,34 @@ class McplinksController < ApplicationController
   def clubs; end
 
   def officers
-    @mcpla_officers = Leader.where(category: 'MCPLA')
-    @dretzka_officers = Leader.where(category: 'Dretzka Park')
-    @oakwood_officers = Leader.where(category: 'Oakwood Park')
-    @whitnall_officers = Leader.where(category: 'Whitnall Park')
-    @browndeer_officers = Leader.where(category: 'Browndeer Park')
-    @currie_officers = Leader.where(category: 'Currie Park')
-    @greenfield_officers = Leader.where(category: 'Greenfield Park')
-    @grant_officers = Leader.where(category: 'Grant Park')
+    #mcpla officers
+    @mcpla_president = Leader.where(category: 'MCPLA').where(title: 'President').first
+    @mcpla_vice_president = Leader.where(category: 'MCPLA').where(title: 'Vice President').first
+    @mcpla_secretary = Leader.where(category: 'MCPLA').where(title: 'Secretary').first
+    @mcpla_treasurer = Leader.where(category: 'MCPLA').where(title: 'Treasurer').first
+    @mcpla_second_team_director = Leader.where(category: 'MCPLA').where(title: '2nd Team Director').first
+    @mcpla_nimaga = Leader.where(category: 'MCPLA').where(title: 'NIMAGA Tournament Director').first
+    @mcpla_tournament_director = Leader.where(category: 'MCPLA').where(title: 'MCPLA Tournament Director').first
+    @mcpla_past_president = Leader.where(category: 'MCPLA').where(title: 'Past President').first
+    @mcpla_committeeman = Leader.where(category: 'MCPLA').where(title: 'USGA Committeeman').first
+
+    #club presidents
+    @browndeer_president = Leader.where(category: 'Browndeer Park').where(title: 'President').first
+    @currie_president = Leader.where(category: 'Currie Park').where(title: 'President').first
+    @dretzka_president = Leader.where(category: 'Dretzka Park').where(title: 'President').first
+    @grant_president = Leader.where(category: 'Grant Park').where(title: 'President').first
+    @greenfield_president = Leader.where(category: 'Greenfield Park').where(title: 'President').first
+    @oakwood_president = Leader.where(category: 'Oakwood Park').where(title: 'President').first
+    @whitnall_president = Leader.where(category: 'Whitnall Park').where(title: 'President').first
+
+    #club treasurers
+    @browndeer_treasurer = Leader.where(category: 'Browndeer Park').where(title: 'Treasurer').first
+    @currie_treasurer = Leader.where(category: 'Currie Park').where(title: 'Treasurer').first
+    @dretzka_treasurer = Leader.where(category: 'Dretzka Park').where(title: 'Treasurer').first
+    @grant_treasurer = Leader.where(category: 'Grant Park').where(title: 'Treasurer').first
+    @greenfield_treasurer = Leader.where(category: 'Greenfield Park').where(title: 'Treasurer').first
+    @oakwood_treasurer = Leader.where(category: 'Oakwood Park').where(title: 'Treasurer').first
+    @whitnall_treasurer = Leader.where(category: 'Whitnall Park').where(title: 'Treasurer').first
   end
 
   def contact; end
