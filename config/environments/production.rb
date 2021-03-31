815c92207226f5087966c26f5976f8edd80d615a
waders92 <wade.carlson@pga.com>
2021-03-31 18:40:41 -0500

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,16 +10,6 @@ Rails.application.configure do
       enable_starttls_auto: true
    }
 
-   ActionMailer::Base.smtp_settings = {
-    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    :password => '<SENDGRID_API_KEY>', # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'yourdomain.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-
   config.action_mailer.default_url_options = { host: 'https://mcplawi.herokuapp.com/' }
 
   # Settings specified here will take precedence over those in config/application.rb.
